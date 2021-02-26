8b2606ff3635eef4a7e0fe8cefff3032a549c74c
jahnavi0102 <jjain.rjc@gmail.com>
2021-02-26 17:10:09 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ function App() {
   const filterHandler = () => {
     switch (status){
       case "completed":
-        setFilteredTodos(todos.fiter((todo) => todo.completed === true));
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
         break;
       case "uncompleted":
-        setFilteredTodos(todos.fiter((todo) => todo.completed === false));
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
         break;  
       default:
         setFilteredTodos(todos);
         break;  
-    }
-  }
+    };
+  };
   return (
     <div className="App">
       <header>
